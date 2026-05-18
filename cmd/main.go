3ac3cec1c96cae9f1f95fb72265325486a1a5e92
Te <3323811499@qq.com>
2026-05-18 19:14:23 +0800

    延时停机

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
 	"order-payment-system/internal/app"
 	"order-payment-system/pkg/logger"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"go.uber.org/zap"
 )
@@ -14,12 +22,29 @@ func main() {
 	logger.Log.Info("app starting...")
 
 	appl, port := app.InitializeApp()
-
 	r := app.SetupRoutes(appl)
-
 	app.Start(appl)
-
 	logger.Log.Info("http server starting", zap.String("port", port))
-	r.Run(":" + port)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+	go func() {
+		logger.Log.Info("http server starting", zap.String("port", port))
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	fmt.Println("Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 25*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown Err:", err)
+	}
 
 }
